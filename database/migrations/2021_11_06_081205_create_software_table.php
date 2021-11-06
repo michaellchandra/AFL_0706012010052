adfcd7d34053b30e3ab74601f9b0e0c6ab293eb0
michaellchandra <chandramichaell93@gmail.com>
2021-11-06 16:35:39 +0800

    initial commit

---
+++ database/migrations/2021_11_06_081205_create_software_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSoftwareTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('software', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('name');
+            $table->string('code');
+            $table->string('platform');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('software');
+    }
+}
