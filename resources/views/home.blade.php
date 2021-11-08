e18cdce5e7b4c6ce134d969ba51570f5169dd3d4
michaellchandra <chandramichaell93@gmail.com>
2021-11-08 13:34:52 +0800

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
-    @extends('layout.mainlayout')
+<body class="bg-primary" >
+    @include('layout.navigation')
     @section('title')
-
-    @section('main_content')
-    <div class="container">
-        <h1 class="">Computer</h1>
+    
+    <div class="jumbotron h-100 bg-gradient text-center text-light">
+        <h1>WELCOME</h1>
+        <p>Computer Software Seller</p>
     </div>
+    @section('main_content')
+    
     @endsection
     
+    <footer>
+        <div class="justify-content-center fixed-bottom text-center p-3 text-light" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2021 Copyright:
+            <a class="text-dark" href="https://macefs.com/">Michael Chandra</a>
+          </div>
+    </footer>
     
 </body>
 </html>
